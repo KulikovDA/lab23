0eab1d719b013f1c7b5d7ce726148645ee539b4a
Arion <Arion@DESKTOP-9BU80RS>
2023-12-24 18:57:52 +0200

    Добавьте файлы проекта.

---
+++ lab23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите А:");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите В:");
+int B = int.Parse(Console.ReadLine());
+if ((A > 0 && B < 0) || (A < 0 && B > 0))
+{
+    A = -A;
+    B = -B;
+}
+else
+{
+    A = 0;
+    B = 0;
+}
+
+Console.WriteLine("A = " + A);
+Console.WriteLine("B = " + B);
